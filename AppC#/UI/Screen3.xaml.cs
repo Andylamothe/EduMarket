1034c604631f14c1765d846eda6907797e0498ed
ManasseTegGbegnohou <157743070+ManasseTegGbegnohou@users.noreply.github.com>
2025-04-18 16:29:20 -0400

    Avancement sur la navigation des pages Home, Sign In, Inventory, Calendar

---
+++ AppC#/UI/Screen3.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace UI
+{
+    public partial class Screen3 : Page
+    {
+        public Screen3()
+        {
+            InitializeComponent();
+        }
+    }
+} 
---
+++ AppC#/UI/Screen4.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace UI
+{
+    public partial class Screen4 : Page
+    {
+        public Screen4()
+        {
+            InitializeComponent();
+        }
+    }
+} 

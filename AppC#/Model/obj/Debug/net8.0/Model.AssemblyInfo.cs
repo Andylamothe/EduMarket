b2e699189f840f3d3f587b0718cd61685ab3a9dc
Marc-AntoineMercier <marc.mercier.mam@gmail.com>
2025-04-19 16:54:29 -0400

    modification comment database create

--- AppC#/Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ AppC#/Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c708c00317e3550c2ce8f6280836d8f8ae1d4679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce6646305f27abd98d15442de4cda87d576a1216")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

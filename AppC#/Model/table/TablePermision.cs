35dd8d19d2efc03b6b67e3acc81393139bc3c8c4
Marc-Antoine Mercier <6240201@collegemv.qc.ca>
2025-04-25 09:59:41 -0400

    remettre fonctionnel

---
+++ AppC#/Model/table/TablePermision.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.table
+{
+    /*
+     * Cette classe va etre utiliser pour mettre un groupe a un utilisateur
+     */ 
+    public class Groupe
+    {
+        [Key]
+        public int IdGroupe { get; set; }
+
+        public required string Name { get; set; }
+
+        public virtual required ICollection<UserModel> Users { get; set; }
+        public virtual required ICollection<GroupePermission> GroupePermissions { get; set; }
+    }
+
+    public class Permission
+    {
+        [Key]
+        public int IdPermission { get; set; }
+
+        public bool CanRead { get; set; }
+
+        public bool CanWrite { get; set; }
+
+        public bool CanUpdate { get; set; }
+
+        public virtual required ICollection<GroupePermission> GroupePermissions { get; set; }
+    }
+
+    public class GroupePermission
+    {
+        [Key]
+        public int IdGroupPermission { get; set; }
+
+        public int IdGroupe { get; set; }
+        [ForeignKey("IdGroupe")]
+        public virtual required Groupe Groupe { get; set; }
+
+        public int IdPermission { get; set; }
+        [ForeignKey("IdPermission")]
+        public virtual required Permission Permission { get; set; }
+
+
+    }
+}
---
+++ AppC#/UI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NavigateToScreen1(null, null);
+        }
+
+        private void NavigateToScreen1(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Screen1());
+        }
+
+        private void NavigateToScreen2(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Screen2());
+        }
+
+        private void NavigateToScreen3(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Screen3());
+        }
+
+        private void NavigateToScreen4(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Screen4());
+        }
+    }
+}

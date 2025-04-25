49e25142088d977457cabae82b68375ddd9bdb39
Marc-Antoine Mercier <6240201@collegemv.qc.ca>
2025-04-25 09:52:22 -0400

    remettre les branch correct

---
+++ AppC#/Model/table/TableCatalogue.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.table
+{
+    public class Catalogue
+    {
+        [Key]
+        public int CatalogueId {  get; set; }
+
+        public int ItemId { get; set; }
+
+        [ForeignKey(nameof(ItemId))]
+        public virtual required Item Item { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual required UserModel Users { get; set; }
+
+    }
+}
---
+++ AppC#/Model/table/TableUtilisateur.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Model.table
+{
+    /*
+     * Cette classe est la classe de base pour tout les types d'utilisateur.
+     * 
+     * L'attribut Reduction est la pour definir la reduction de chaque utilisateur, selon leur achat qu'il vont faire.
+     * 
+     * utilisation d'une classe abstract pour ne pas pouvoir instancier un UserModel
+     * (il n'est pas possible de faire new UserModel)(impossible)
+     */
+    public abstract class UserModel
+    {
+        [Key]
+        public int IdUser { get; set; }
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Phone { get; set; }
+
+        [Required]
+        public required string Login { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+
+        [Range(0, 100, ErrorMessage = "La reduction doit être entre 0 et 100.")]
+        public float Reduction { get; set; }
+
+        public int GroupId { get; set; }
+
+        [ForeignKey(nameof(GroupId))]
+        public virtual required Groupe GroupeUser { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+
+        public override string ToString()
+        {
+            return "UserModel{" +
+                "IdUser: " + this.IdUser +
+                ", FirstName: " + this.FirstName +
+                ", LastName: " + this.LastName +
+                ", Email: " + this.Email +
+                ", Phone: " + this.Phone +
+                ", Login: " + this.Login +
+                ", Password: " + this.Password +
+                ", Reduction: " + this.Reduction +
+                ", IsActive: " + this.IsActive +
+                "}";
+        }
+    }
+
+    public class Admin : UserModel
+    {
+
+        public Admin()
+        {
+            Reduction = 0;
+        }
+
+
+    }
+
+    public class Departement : UserModel
+    {
+        public Departement()
+        {
+            Reduction = 100;
+        }
+    }
+
+    public class Teacher : UserModel
+    {
+
+        public Teacher()
+        {
+            Reduction = 15;
+        }
+    }
+
+    public class Student : UserModel
+    {
+
+        public Student()
+        {
+            Reduction = 30;
+        }
+
+    }
+
+    public static class DiscriminantTableUtilisateur
+    {
+        public static readonly string ADMIN = "admin";
+        public static readonly string STUDENT = "student";
+        public static readonly string TEACHER = "teacher";
+        public static readonly string DEPARTEMENT = "departement";
+    }
+}

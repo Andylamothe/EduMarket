926d87d52a848d36510fd17ade6db5fef465a2e5
“ManasseTegGbegnohou” <mtgbegnohou@gmail.com>
2025-05-03 19:43:50 -0400

    petit clean up pour comfort visuel

--- AppC#/UI/obj/Debug/net8.0-windows/UI.AssemblyInfo.cs
+++ AppC#/UI/obj/Debug/net8.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687c7c798bf32cacce4a88b32ce0b690a63bc6df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce673c8035d8fdb8799074e7836c4a66ea6faf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

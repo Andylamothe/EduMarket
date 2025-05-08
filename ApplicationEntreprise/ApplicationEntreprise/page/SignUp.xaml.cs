751482bdd8a1026213a0e5890850b6ba08b96f78
NWilliRex <panda16520@gmail.com>
2025-05-08 12:46:44 -0400

    add the new project (William)

---
+++ ApplicationEntreprise/ApplicationEntreprise/page/SignUp.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using ViewModel.navigationService;
+using ViewModel.viewmodel;
+
+namespace WpfApplication.page
+{
+    /// <summary>
+    /// Logique d'interaction pour SignUp.xaml
+    /// </summary>
+    public partial class SignUp : UserControl
+    {
+        private readonly SignUpViewModel<ApplicationPage, UserControl> _viewModel;  
+        public SignUp(SignUpViewModel<ApplicationPage, UserControl> viewModel)
+        {
+            InitializeComponent();
+            _viewModel = viewModel;
+            DataContext = _viewModel;
+        }
+    }
+}
---
+++ ApplicationEntreprise/ViewModel/viewmodel/SignInViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ViewModel.messageService;
+using ViewModel.navigationService;
+
+namespace ViewModel.viewmodel;
+
+public partial class SignInViewModel<TPage, T> : BaseViewModel<TPage, T> where TPage : struct, Enum
+{
+    private readonly MessageHandler<string> _handler = new();
+
+    [ObservableProperty]
+    private string username;
+
+    public SignInViewModel(INavigationService<TPage, T> navigationService) : base(navigationService)
+    {
+        _handler.RegisterChannel("username", (_, msg) => Username = msg)
+            .Listen(this);
+    }
+}

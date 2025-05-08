f26c54fa2a99cfbc1f7875745a0a545d1ef084ba
Ha-Vu-William Nguyen <152650970+NWilliRex@users.noreply.github.com>
2025-05-08 16:17:15 -0400

    T00001 faire la base de donne (#19)
    * remettre t00001 comme avant
    
    * remettre le code ui
    
    * remettre avec toute les modifications
    
    * modificationj nom attribut model pour qu'il suive un certain logique
    
    * modificationj nom attribut model pour qu'il suive un certain logique
    
    * modification pour relation one to one avec permission
    
    * modification pour relation one to one avec permission
    
    * ajouter la base de donne (William)
    
    * faire les viewModel (William)
    
    * ajouter base de donne fonctionnel (William)
    
    ---------
    
    Co-authored-by: Marc-Antoine Mercier <6240201@collegemv.qc.ca>
    Co-authored-by: Marc-AntoineMercier <marc.mercier.mam@gmail.com>

---
+++ ApplicationEntreprise/Model/DataBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.table;
+
+namespace Model;
+public class DataBaseContext : DbContext
+{
+    /*
+        * DbSet sont les tables de ma base de donnees et ces ceux-ci qu'on va utiliser dans les ViewModels. 
+        */
+    public DbSet<UserModel> Users { get; set; }
+
+    public DbSet<Item> Items { get; set; }
+
+    public DbSet<Transaction> Transactions { get; set; }
+
+    public DbSet<Catalogue> Catalogues { get; set; }
+
+    public DbSet<Groupe> Groupes { get; set; }
+
+    public DbSet<Permission> Permissions { get; set; }
+
+    /*
+        * On appelle le constructeur de la classe parente
+        * 
+        * pour initialiser le contexte de la base de données.
+        */
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=test.db");
+        optionsBuilder.UseLazyLoadingProxies();
+    }
+
+    /*
+        * Cette méthode est appelée lors de la création du modèle de données.
+        * En gros, cette methodes est utiliser pour configurer le modèle de données.
+        * Ces a ce moment la que je definit les tables et relation de ma base de donnees.
+        */
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserModel>()
+            .ToTable("Utilisateur")
+            .HasDiscriminator<string>("Role")
+            .HasValue<Admin>(DiscriminantTableUtilisateur.ADMIN)
+            .HasValue<Teacher>(DiscriminantTableUtilisateur.TEACHER)
+            .HasValue<Student>(DiscriminantTableUtilisateur.STUDENT)
+            .HasValue<Departement>(DiscriminantTableUtilisateur.DEPARTEMENT);
+
+        modelBuilder.Entity<Groupe>().ToTable("Groupe");
+
+        modelBuilder.Entity<Permission>().ToTable("Permission");
+
+        modelBuilder.Entity<Item>().ToTable("Item");
+
+        modelBuilder.Entity<Transaction>().ToTable("Transaction");
+
+        modelBuilder.Entity<Catalogue>().ToTable("Catalogue");
+    }
+}
---
+++ ApplicationEntreprise/Model/table/TablePermision.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.table
+{
+    /*
+     * Cette classe va etre utiliser pour mettre un groupe a un utilisateur
+     */ 
+    public class Groupe
+    {
+        [Key]
+        public int GroupeId { get; set; }
+
+        public required string Name { get; set; }
+
+        public virtual required ICollection<UserModel> Users { get; set; }
+
+        public int PermissionId { get; set; }
+
+        [ForeignKey("IdPermission")]
+        public virtual required Permission Permission { get; set; }
+    }
+
+    public class Permission
+    {
+        [Key]
+        public int PermissionId { get; set; }
+
+        public bool CanRead { get; set; }
+
+        public bool CanWrite { get; set; }
+
+        public bool CanUpdate { get; set; }
+
+    }
+}
